e186ed13982160073cf3bc456461672838f389c5
chipembere <chitiyobrian@gmail.com>
2019-11-19 17:00:20 +0000

    Updated missing files.

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express'); //$ npm install express -save   done
-const app = express(); 
-const port = 3000; 
+const app = express();
+const port = 3000;
 
 app.use(express.static('public'));
+app.use(express.static('images'));
 app.engine('html', require('ejs').renderFile); //$ npm install ejs -save    done
 
 
-app.get('/', (req, res) => { 
-    res.render("index.html") 
-   })
+app.get('/', (req, res) => {
+   res.render("index.html")
+});
 
-app.post('/results', (req, res) => { 
-   res.render("results.html") 
+app.post('/results', (req, res) => {
+
+   res.render("results.html")
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
