29ef7fd6c1069235cc344e0ba9be47e3de955ea5
Aaron M <aaron_213@hotmail.co.uk>
2019-11-19 12:51:51 +0000

    google search method added but second page not working

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express'); //$ npm install express -save   done
+const app = express(); 
+const port = 3000; 
+
+app.use(express.static('public'));
+app.engine('html', require('ejs').renderFile); //$ npm install ejs -save    done
+
+
+app.get('/', (req, res) => { 
+    res.render("index.html") 
+   })
+
+app.post('/results', (req, res) => { 
+   res.render("results.html") 
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
+
+
