2bcb6e2ed099102f0eba3f3d87326629ddaff48d
chipembere <chitiyobrian@gmail.com>
2019-11-20 11:13:10 +0000

    Update the main.js before pulling.

--- public/main.js
+++ public/main.js
@@ -1,21 +1,24 @@
 $(document).ready(function () {
     $("#search").click(function () {
         let query = $("input[name=bar]").val();
-        // console.log(query);
+        console.log(`Query: ${query}`);
         const authKey = "key=AIzaSyAP9tf1sdBxzqUHpiNNHFxGI19Fa8-pR70";
         let mainURL = "https://www.googleapis.com/customsearch/v1?";
         let numb = "start=0&num=10";
         let cx = "&cx=017052284145539921913:vwovdaiwm3d";
-        let url = `${mainURL}${authKey}${cx}&q=${query}${numb}`;
-        let url1 = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAP9tf1sdBxzqUHpiNNHFxGI19Fa8-pR70&cx=017052284145539921913:vwovdaiwm3d&q=audi";
-        // console.log(`URL ${url}`)
-        fetch(url1).then(function (response) {
+        let url1 = `${mainURL}${authKey}${cx}&q=${query}${numb}`;
+        let url = `https://www.googleapis.com/customsearch/v1?key=AIzaSyAP9tf1sdBxzqUHpiNNHFxGI19Fa8-pR70&cx=017052284145539921913:vwovdaiwm3d&q=${query}`;
+        console.log(`URL ${url}`);
+        fetch(url).then(function (response) {
             return response.json();
         }).then(function (response) {
+            console.log('Response' + response.items)
             for (var i = 0; i < response.items.length; i++) {
                 var item = response.items[i];
+                console.log(`Items: ${item}`)
                 // in production code, item.htmlTitle should have the HTML entities escaped.
-                ("searchresults").innerHTML += "<br>" + item.htmlTitle;
+                document.getElementById("searchresults").innerHTML = item;
+                // document.getElementById("searchresults").innerHTML = item.htmlTitle;
             }
         });
     });
